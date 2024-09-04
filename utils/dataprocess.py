bd61c7ba1856f41a3f2bce782645498e6451613b
Boyi Liu <byliu0325@gmail.com>
2024-09-04 14:23:36 +0800

    update dataprocess.py into numpy

--- utils/dataprocess.py
+++ utils/dataprocess.py
@@ -3,26 +3,26 @@ import numpy as np
 
 class DataProcessor:
     def __init__(self):
-        self.data = []
+        self.data = np.array([])
 
     def append(self, item, times=1):
         for _ in range(times):
-            self.data.append(item)
+            self.data = np.append(self.data, item)
         return
 
     def avg(self, p=0):
         if self.is_empty():
             return -1
-        return sum(self.data[p:]) / len(self.data[p:])
+        return np.mean(self.data[p:])
 
     def std(self, p=0):
         return -1 if self.is_empty() else np.std(self.data[p:])
 
     def min(self, p=0):
-        return min(self.data[p:])
+        return np.min(self.data[p:])
 
     def max(self, p=0):
-        return max(self.data[p:])
+        return np.max(self.data[p:])
 
     def last(self):
         return -1 if self.is_empty() else self.data[-1]
@@ -31,4 +31,4 @@ class DataProcessor:
         self.data = []
 
     def is_empty(self):
-        return self.data == []
+        return self.data.size == 0
